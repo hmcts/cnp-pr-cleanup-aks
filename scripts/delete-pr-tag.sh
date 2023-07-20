fa786885ca69dfc493e8d51460b8022267908742
Rees Pozzi <reespozzi@icloud.com>
2023-07-20 13:55:16 +0100

    Add check for valid labels before curlgs

--- scripts/delete-pr-tag.sh
+++ scripts/delete-pr-tag.sh
@@ -10,12 +10,11 @@ REPO_URL=$(echo "${PR_API_URL%/*/*}")
 if [[ -z "$RELEASE_NAME_LABEL" || -z "$PR_API_URL" || -z "$GIT_TOKEN" ]]; then
     echo "Variables are not set - PR potentiall had no labels. Skipping..."
 else
-    echo "Deleting ${RELEASE_NAME_LABEL} label"
-
     curl -i -L \
         -X DELETE \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GIT_TOKEN}"\
         -H "X-GitHub-Api-Version: 2022-11-28" \
         ${REPO_URL}/labels/${RELEASE_NAME_LABEL}
+    echo "Deleted ${RELEASE_NAME_LABEL} label"
 fi
