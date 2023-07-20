0cafee64da6806bcf5ddf14218aabaf563cfcf3a
Rees Pozzi <reespozzi@icloud.com>
2023-07-20 13:33:11 +0100

    Move script into external step

---
+++ scripts/delete-pr-tag.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+
+RELEASE_NAME_LABEL=${1}
+PR_API_URL=${2}
+GIT_TOKEN=${3}
+
+REPO_URL=$(echo "${PR_API_URL%/*/*}")
+echo "Deleting ${RELEASE_NAME_LABEL} label"
+
+curl -L \
+    -X DELETE \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${GIT_TOKEN}"\
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    ${REPO_URL}/labels/${RELEASE_NAME_LABEL}
