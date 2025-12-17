e18d511bc761e5a2b3c7fabbdaa7cfd8d32a9ff1
Jake Moorhead <jake.moorhead@justice.gov.uk>
2025-12-17 16:47:23 +0000

    Update to remove the non live ACRs and the matrix logic

--- scripts/untag-acr-image.sh
+++ scripts/untag-acr-image.sh
@@ -2,14 +2,18 @@
 
 RELEASE_NAME=${1}
 PRODUCT=${2}
-CONTAINER_REGISTRY=${3}
+CONTAINER_REGISTRIES=${3}  # Space-separated list of ACR names
 
 component=$(echo ${RELEASE_NAME} | sed -e "s/^${PRODUCT}-//" -e 's/-pr-.*//')
 repository="${PRODUCT}/${component}"
 tag=$(echo ${RELEASE_NAME} | sed "s/.*-pr-/pr-/")
 
-echo "Deleting $repository:$tag"
+echo "Deleting $repository:$tag from multiple ACRs"
 
-az acr repository show-tags -n ${CONTAINER_REGISTRY} --repository $repository --query "[?starts_with(@, '$tag')]" -o tsv \
-| xargs -I% az acr repository untag -n ${CONTAINER_REGISTRY} --image "$repository:%"
+for CONTAINER_REGISTRY in ${CONTAINER_REGISTRIES}; do
+  echo "Processing ${CONTAINER_REGISTRY}..."
+  az acr repository show-tags -n ${CONTAINER_REGISTRY} --repository $repository --query "[?starts_with(@, '$tag')]" -o tsv \
+  | xargs -I% az acr repository untag -n ${CONTAINER_REGISTRY} --image "$repository:%" 2>/dev/null || echo "No tags found in ${CONTAINER_REGISTRY}"
+done
+
 echo "Deleted $repository:$tag"
